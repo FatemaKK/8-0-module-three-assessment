69245bc9e51df7add992fdcef1428767c4b8fdef
Fatema Kabir <fatema.kabir@pursuit.org>
2021-12-16 14:57:08 -0500

    display people

--- src/components/People.js
+++ src/components/People.js
@@ -3,10 +3,45 @@ import "../App.css";
 import "./People.css"
 
 class People extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      people: [],
+    };
+  }
+
+  fetchPeople = () => {
+    fetch("https://ghibliapi.herokuapp.com/people")
+      .then((res) => res.json()) 
+      .then((data) => {
+        this.setState({
+          people: data,
+        });
+      });
+  };
+
+  componentDidMount = () => {
+    this.fetchPeople();
+  };
+
   render() {
-    return <h1 className="people">Search for a Person</h1>;
+    let display = this.state.people.map((person) => {
+      return (
+        <div className="person">
+          <h2>Name: {person.name} </h2>
+          <h3>Age: {person.age} </h3>
+          <h3>Gender: {person.gender} </h3>
+        </div>
+      );
+    });
+    return (
+      <div className="locations">
+        <h1>Search for a Person</h1>
+        <div className="peopleList">{display}</div>
+      </div>
+    );
   }
 }
 
 export default People;
-
